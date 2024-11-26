b498fe56a9201de435f3c2b78b837616ee1b3e43
Dimitrios Psaroudakis <73707939+Mitsos132005@users.noreply.github.com>
2024-11-26 22:50:41 +0100

    Add files via upload

---
+++ StartStopNotepad.ps1
@@ -0,0 +1,10 @@
+﻿$processes = Get-Process | Where-Object { $_.Name -eq "*note*" }
+if($null -eq $processes)
+{
+    Start-Process notepad
+}
+$a = Get-Process *note*
+if( (Stop-Process $a -Confirm) -eq $true)
+{
+    Write-Host "Process stopped"
+}
